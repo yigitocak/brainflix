ad4816609ec4571bf68f3c76f45108338826f187
yigit ocak <yigitockk@gmail.com>
2024-04-03 23:53:26 -0400

    mobile version of the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,31 @@
 import "./Header.scss"
 import logo from "../../assets/logo/BrainFlix-logo.svg"
+import upload from "../../assets/icons/upload.svg"
 
 const Header = () => {
     return (
-        <header>
+        <header className='header'>
             <img
                 src={logo}
                 alt="BrainFlix Logo"
                 className='header__logo'
             />
-            <form>
-                <input
-                    type="text"
-                    placeholder="Search"
-                    name='header__search-bar'
-                    className="header__search-bar"
-                />
-                <div
-                    className="header__avatar"
-                ></div>
+            <form className="header__form">
+                <div className="header__container">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        name='header__search-bar'
+                        className="header__search-bar"
+                    />
+                    <div
+                        className="header__avatar"
+                    ></div>
+                </div>
                 <button
                     type="submit"
-                >UPLOAD</button>
+                    className="header__button"
+                ><img src={upload} alt="upload icon" className="header__button-image"></img>UPLOAD</button>
             </form>
         </header>
     )
